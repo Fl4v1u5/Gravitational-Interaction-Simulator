97ef17572a3fc9e51c84f3f0e3a5628da8f6d99b
Emanuel Hongu Flavius <fl4v1u5@yandex.com>
2018-12-17 23:33:52 +0100

    Basic basic feature added
    It can now draw a circle on the position of the mouse everytime you click

--- GameController.gd
+++ GameController.gd
@@ -1,14 +1,34 @@
-extends Node
+extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var do_I_draw = false
+var center
+var test_radius = 80
+var test_color = Color(255, 0, 255)
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
+	
 	pass
 
+func _input(event):
+	
+	if event.is_action_pressed("ui_left_mouse"):
+		
+		do_I_draw = true
+		center = get_viewport().get_mouse_position()
+		update()
+		
+	
+	pass
+
+func _draw():
+	
+	if (do_I_draw == true):
+		
+		draw_circle(center, test_radius, test_color)
+		do_I_draw = false
+		
+	
+	pass
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
